610c0b5de16e6911f871b1c84fd3b9b82b876b57
Brandon <bhatch96@gmail.com>
2021-02-24 22:08:54 -0700

    Add project files.

---
+++ Amazon/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Amazon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Models.ViewModels;
+
+namespace Amazon.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private IAmazonRepository _repository;
+
+        public int PageSize = 5;
+
+        public HomeController(ILogger<HomeController> logger, IAmazonRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            return View(new BookListViewModel
+                {
+                    Books = _repository.Books
+                        .OrderBy(p => p.BookID)
+                        .Skip((page-1) * PageSize)
+                        .Take(PageSize)
+                    ,
+                    PagingInfo = new PagingInfo
+                    {
+                        CurrentPage = page,
+                        ItemsPerPage = PageSize,
+                        TotalNumItems = _repository.Books.Count()
+                    }
+                });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
